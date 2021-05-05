30822206809f853404f1cc230be5b728ebe402f3
Sergey <golivkin.s.v@gmail.com>
2021-05-05 15:41:05 +0300

    refactoring print

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -12,16 +12,17 @@ public class Main {
         List<Flight> flights = FlightBuilder.createFlights();
         FlightServiceImpl segmentService = new FlightServiceImpl();
         System.out.println("\nСписок - 1 : Все перелёты и сегменты");
-        System.out.println("************************************\n" + flights);
+        System.out.println("************************************");
+        flights.forEach(System.out::println);
         System.out.println("\nСписок - 2 : Исключить перелеты с вылетом до текущего момента времени");
-        System.out.println("*********************************************************************\n" +
-        segmentService.minusDepartureTimeBeforeCurrentTime(flights));
+        System.out.println("*********************************************************************");
+        segmentService.minusDepartureTimeBeforeCurrentTime(flights).forEach(System.out::println);
         System.out.println("\nСписок - 3 : Исключить перелеты, в которых имеются сегменты с датой прилёта раньше даты вылета");
-        System.out.println("**********************************************************************************************\n" +
-        segmentService.minusFlightArrivalDateBeforeDepartureDate(flights));
+        System.out.println("**********************************************************************************************");
+        segmentService.minusFlightArrivalDateBeforeDepartureDate(flights).forEach(System.out::println);
         System.out.println("\nСписок - 4 : Исключить перелеты, где общее время, проведённое на земле, превышает два часа ");
-        System.out.println("******************************************************************************************\n"
-        + segmentService.minusFlightsWithMoreThanTwoHoursOnTheGround(flights));
+        System.out.println("******************************************************************************************");
+        segmentService.minusFlightsWithMoreThanTwoHoursOnTheGround(flights).forEach(System.out::println);
 
     }
 }
